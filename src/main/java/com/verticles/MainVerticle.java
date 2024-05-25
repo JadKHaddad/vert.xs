99c6c20b257aaecf01fd75291d540657c73286ca
JadKHaddad <jadkhaddad@gmail.com>
2024-05-25 21:25:32 +0200

    extracted handlers

--- src/main/java/com/verticles/MainVerticle.java
+++ src/main/java/com/verticles/MainVerticle.java
@@ -1,12 +1,12 @@
 package com.verticles;
 
-import com.fasterxml.jackson.databind.DatabindException;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.LoggerHandler;
 
@@ -33,69 +33,9 @@ public class MainVerticle extends AbstractVerticle {
 
     router.route()
       .handler(LoggerHandler.create())
-      .handler(BodyHandler.create()) // to get body from request. Otherwise null
-      .handler(context -> { // custom header validation and it also passes the header value to next handler
-        String apiKeyValue = context.request().getHeader(API_KEY_HEADER);
-        if (apiKeyValue == null) {
-          logger.warn("Unauthorized request without API key");
-          context.response().setStatusCode(401).end("Unauthorized: Missing API key");
-          return;
-        }
-
-        if (!apiKeys.contains(apiKeyValue)) {
-          logger.warn(String.format("Unauthorized request with API key %s", apiKeyValue));
-          context.response().setStatusCode(401).end("Unauthorized: Invalid API key");
-          return;
-        }
-
-        context.put(USED_API_KEY, apiKeyValue);
-        context.next();
-      })
-      .handler(context -> { // custom basic auth
-        String authHeader = context.request().getHeader("Authorization");
-        if (authHeader == null) {
-          logger.warn("Unauthorized request without Authorization header");
-          context.response().setStatusCode(401).end("Unauthorized: Missing Authorization header");
-          return;
-        }
-
-        String[] parts = authHeader.split(" ");
-        if (parts.length != 2) {
-          logger.warn("Unauthorized request with invalid Authorization header");
-          context.response().setStatusCode(401).end("Unauthorized: Invalid Authorization header");
-          return;
-        }
-
-        String authType = parts[0];
-        String authValue = parts[1];
-
-        if (!authType.equals("Basic")) {
-          logger.warn("Unauthorized request with invalid Authorization type");
-          context.response().setStatusCode(401).end("Unauthorized: Invalid Authorization type");
-          return;
-        }
-
-        String decodedAuthValue = new String(Base64.getDecoder().decode(authValue));
-        String[] authParts = decodedAuthValue.split(":");
-        if (authParts.length != 2) {
-          logger.warn("Unauthorized request with invalid Authorization value");
-          context.response().setStatusCode(401).end("Unauthorized: Invalid Authorization value");
-          return;
-        }
-
-        String username = authParts[0];
-        String password = authParts[1];
-
-        if (!users.containsKey(username) || !users.get(username).equals(password)) {
-          logger.warn(String.format("Unauthorized request with invalid credentials %s:%s", username, password));
-          context.response().setStatusCode(401).end("Unauthorized: Invalid credentials");
-          return;
-        }
-
-        AuthorizedUser authorizedUser = new AuthorizedUser(username, password);
-        context.put(AUTHORIZED_USER, authorizedUser);
-        context.next();
-      });
+      .handler(this::apiKeyHandler)
+      .handler(this::basicAuthHandler);
+    router.post().handler(BodyHandler.create()); // to get body from request. Otherwise, null. Have to set it on post route or options etc. If set on get route, it will throw error.
 
     // curl -X GET http://localhost:9000/ -H "X-Api-Key: expected-value-1" -i -u user1:password1
     router.get("/")
@@ -133,5 +73,69 @@ public class MainVerticle extends AbstractVerticle {
       .requestHandler(router)
       .listen(9000)
       .onSuccess(server -> logger.info(String.format("HTTP server started on port %d", server.actualPort())));
+  }
+
+  private void apiKeyHandler(RoutingContext context) { // custom header validation and it also passes the header value to next handler
+    String apiKeyValue = context.request().getHeader(API_KEY_HEADER);
+    if (apiKeyValue == null) {
+      logger.warn("Unauthorized request without API key");
+      context.response().setStatusCode(401).end("Unauthorized: Missing API key");
+      return;
+    }
+
+    if (!apiKeys.contains(apiKeyValue)) {
+      logger.warn(String.format("Unauthorized request with API key %s", apiKeyValue));
+      context.response().setStatusCode(401).end("Unauthorized: Invalid API key");
+      return;
+    }
+
+    context.put(USED_API_KEY, apiKeyValue);
+    context.next();
+  }
+
+  private void basicAuthHandler(RoutingContext context) { // custom basic auth validation and it also passes the user to next handler
+    String authHeader = context.request().getHeader("Authorization");
+    if (authHeader == null) {
+      logger.warn("Unauthorized request without Authorization header");
+      context.response().setStatusCode(401).end("Unauthorized: Missing Authorization header");
+      return;
+    }
+
+    String[] parts = authHeader.split(" ");
+    if (parts.length != 2) {
+      logger.warn("Unauthorized request with invalid Authorization header");
+      context.response().setStatusCode(401).end("Unauthorized: Invalid Authorization header");
+      return;
+    }
+
+    String authType = parts[0];
+    String authValue = parts[1];
+
+    if (!authType.equals("Basic")) {
+      logger.warn("Unauthorized request with invalid Authorization type");
+      context.response().setStatusCode(401).end("Unauthorized: Invalid Authorization type");
+      return;
+    }
+
+    String decodedAuthValue = new String(Base64.getDecoder().decode(authValue));
+    String[] authParts = decodedAuthValue.split(":");
+    if (authParts.length != 2) {
+      logger.warn("Unauthorized request with invalid Authorization value");
+      context.response().setStatusCode(401).end("Unauthorized: Invalid Authorization value");
+      return;
+    }
+
+    String username = authParts[0];
+    String password = authParts[1];
+
+    if (!users.containsKey(username) || !users.get(username).equals(password)) {
+      logger.warn(String.format("Unauthorized request with invalid credentials %s:%s", username, password));
+      context.response().setStatusCode(401).end("Unauthorized: Invalid credentials");
+      return;
+    }
+
+    AuthorizedUser authorizedUser = new AuthorizedUser(username, password);
+    context.put(AUTHORIZED_USER, authorizedUser);
+    context.next();
   }
 }
